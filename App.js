12e3990a6212b4d66d57f827793b77c795decaee
MubashirLabar <mubashirnazir115@gmail.com>
2025-10-21 15:21:52 +0500

    multi style

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet, StatusBar, ScrollView } from "react-native";
 
-export default function App() {
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+      <View style={styles.container}>
+        <View style={[styles.box, { backgroundColor: "green" }]}>
+          <Text style={styles.title}>Pizza</Text>
+        </View>
+        <View style={[styles.box, { backgroundColor: "red" }]}>
+          <Text style={styles.title}>Burger</Text>
+        </View>
+        <View style={[styles.box, { backgroundColor: "pink" }]}>
+          <Text style={styles.title}>Pasta</Text>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    gap: 20,
+    paddingVertical: 80,
+    paddingHorizontal: 20,
+  },
+  box: {
+    width: 200,
+    height: 250,
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#000",
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: "bold",
+    color: "white",
   },
 });
